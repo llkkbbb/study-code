57b317aeeecd7f131960f6ad7003dd1ef20f025a
이경범 <tkdwk567@naver.com>
2022-06-24 11:54:27 +0900

    java - serialization study

---
+++ src/main/java/com/example/blogcode/serialization/직렬화/User.java
@@ -0,0 +1,39 @@
+package com.example.blogcode.serialization.직렬화;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * packageName    : com.example.blogcode.serialization
+ * fileName       : User
+ * author         : tkdwk567@naver.com
+ * date           : 2022/06/23
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class User implements Serializable {
+    // Serializable 을 구현한 클래스를 상속받는다면 상속받은 클래스는 Serializable 을 구현하지 않아도 직렬화가 가능하다.
+
+    // 1. SerialVersionUID 으로 클래스의 버전을 관리한다.
+    // 2. 해당값이 없으면 기본으로 해쉬값으로 관리된다.
+    // 3. 직렬화를 한 상태에서 해당 객체의 필드를 추가 즉, 버전이 변경되면 InvalidClassException 발생! 그러므로 SerialVersionUID 으로 관리 되어야 한다.
+//    private static final long serialVersionUID = -4255938668293970668L;
+
+    private Long id;
+    private String name;
+    // 새로운 필드 추가
+//    private String firstName;
+    private int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+}
---
+++ src/test/java/com/example/blogcode/serialization/직렬화/ObjectSerializationTest.java
@@ -0,0 +1,54 @@
+package com.example.blogcode.serialization.직렬화;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+
+/**
+ * packageName    : com.example.blogcode.serialization.직렬화
+ * fileName       : ObjectSerializationTest
+ * author         : tkdwk567@naver.com
+ * date           : 2022/06/24
+ */
+class ObjectSerializationTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("직렬화 과정")
+    void serialization() throws IOException {
+        User user = new User(1L, "name", 10);
+
+        String base64User = SerializationFactory.serialization(user);
+
+        System.out.println(base64User);
+    }
+
+    @Test
+    @DisplayName("역직렬화 과정 테스트 -> User 클래스 버전이 달라지면 InvalidClassException 에러 발생")
+    byte[] deserialization() throws IOException, ClassNotFoundException {
+        String base64User = "rO0ABXNyADFjb20uZXhhbXBsZS5ibG9nY29kZS5zZXJpYWxpemF0aW9uLuyngeugrO2ZlC5Vc2Vybi14ikL8dX4CAANJAANhZ2VMAAJpZHQAEExqYXZhL2xhbmcvTG9uZztMAARuYW1ldAASTGphdmEvbGFuZy9TdHJpbmc7eHAAAAAKc3IADmphdmEubGFuZy5Mb25nO4vkkMyPI98CAAFKAAV2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAAAAAABdAAEbmFtZQ==";
+        byte[] deserializationArr = Base64.getDecoder().decode(base64User);
+        User user = (User) SerializationFactory.deserialization(deserializationArr);
+        String json = objectMapper.writeValueAsString(user);
+
+        System.out.println("user Json size : " + json.getBytes().length);
+//        System.out.println(user.toString());
+        return deserializationArr;
+    }
+
+    @Test
+    @DisplayName("용량 테스트")
+    void sizeTest() throws IOException, ClassNotFoundException {
+        byte[] deserializationArr = deserialization();
+        System.out.println(deserializationArr.length);
+    }
+
+}
