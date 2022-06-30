fc98b7f46ea4c71a0544bc3cce4a41e552bc1ea9
이경범 <tkdwk567@naver.com>
2022-07-01 00:45:46 +0900

    java abstract class study 추상 클래스

---
+++ src/main/java/com/example/blogcode/abstractstudy/Car.java
@@ -0,0 +1,14 @@
+package com.example.blogcode.abstractstudy;
+
+/**
+ * packageName    : com.example.blogcode.abstractstudy
+ * fileName       : Car
+ * author         : tkdwk567@naver.com
+ * date           : 2022/07/01
+ */
+public abstract class Car {
+
+    // fields....
+
+    public abstract void modeChange();
+}
---
+++ src/main/java/com/example/blogcode/abstractstudy/Mornning.java
@@ -0,0 +1,15 @@
+package com.example.blogcode.abstractstudy;
+
+/**
+ * packageName    : com.example.blogcode.abstractstudy
+ * fileName       : Mornning
+ * author         : tkdwk567@naver.com
+ * date           : 2022/07/01
+ */
+public class Mornning extends Car{
+
+    @Override
+    public void modeChange() {
+
+    }
+}
