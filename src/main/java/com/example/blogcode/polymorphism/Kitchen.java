d50aa3ff80305691c5a776a546f19e35e4093cef
이경범 <tkdwk567@naver.com>
2022-06-30 19:22:24 +0900

    java polymorphism study 다형성

---
+++ src/main/java/com/example/blogcode/polymorphism/Kitchen.java
@@ -0,0 +1,15 @@
+package com.example.blogcode.polymorphism;
+
+/**
+ * packageName    : com.study.abstract1.다형성
+ * fileName       : Kitchen
+ * author         : tkdwk567@naver.com
+ * date           : 2022/06/30
+ */
+public class Kitchen extends Employee {
+
+    @Override
+    public void work() {
+        System.out.println("음식 만들기");
+    }
+}
