0649b4bfea9262edf82a2899c02212faab35a91b
이경범 <tkdwk567@naver.com>
2022-07-07 00:37:01 +0900

    bus 도메인 하차, 승객수 반환, 환승 체크 메서드 추가

--- src/main/java/com/example/blogcode/project/domain/Bus.java
+++ src/main/java/com/example/blogcode/project/domain/Bus.java
@@ -32,9 +32,39 @@ public class Bus {
         this.number = number;
     }
 
+    // 승차
     public Member getOn(Member member) {
         this.members.add(member);
         return member;
     }
 
+    // 하차
+    public Member getOff(Member member) {
+        for (Member m : members) {
+            if (m.getId() == member.getId()) {
+                members.remove(member);
+                break;
+            }
+        }
+        return member;
+    }
+
+    // 승객수 반환
+    public int getPassengers() {
+        return this.members.size();
+    }
+
+    // 환승 체크
+    public boolean checkTransfer(Member member) {
+        if (isTransfer(member)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isTransfer(Member member) {
+        return member.getGetOffTheBusDateTime() != null
+                && member.getGetOnTheBusDateTime().isBefore(member.getGetOffTheBusDateTime().plusSeconds(10));
+    }
 }
