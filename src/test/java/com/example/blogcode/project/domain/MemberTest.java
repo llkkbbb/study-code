714d6ace07b51ba3944337581a7969a11ac08ec1
이경범 <tkdwk567@naver.com>
2022-07-07 00:38:35 +0900

    member 도메인 테스트 및 리펙토링 멤버 객채 생성, 승츠 테스트, 잔액 부족 테스트, 버스 하차 테스트

--- src/test/java/com/example/blogcode/project/domain/MemberTest.java
+++ src/test/java/com/example/blogcode/project/domain/MemberTest.java
@@ -3,6 +3,8 @@ package com.example.blogcode.project.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,56 +20,77 @@ class MemberTest {
     @Test
     @DisplayName("멤버 객체 생성")
     void MemberTest() {
-        Member member = Member.builder()
-                .id(1L)
-                .name("스폰지밥")
-                .money(10000)
-                .build();
+        // given
+        int money = 10000; // 멤버 생성
 
+        // when
+        Member member = createMember(money);
+
+        // then
         assertNotNull(member);
-
+        assertThat(member.getMoney()).isEqualTo(money);
     }
 
     @Test
-    @DisplayName("버스 요금 지불 성공")
+    @DisplayName("버스 승차 테스트 -> 잔액, 승차 시간, 승객 수")
     void getOnBus_success() {
-        // 버스 생성
-        Bus bus900 = Bus.builder().id(1L)
-                .name("마을버스")
-                .number(900)
-                .build();
+        // given
+        Bus bus = createBus(); // 버스 생성
+        Member member = createMember(10000); // 멤버 생성
 
-        // 멤버 생성
-        Member member = Member.builder()
-                .id(1L)
-                .name("스폰지밥")
-                .money(10000)
-                .build();
+        // when
+        Member busMember = member.getOnBus(bus); // 버스 승차
 
-        Member busMember = member.getOnBus(bus900);
-
-        assertThat(busMember.getMoney()).isEqualTo(member.getMoney());
+        // then
+        assertThat(busMember.getMoney()).isEqualTo(member.getMoney()); // 잔액 검증
+        assertThat(busMember.getGetOnTheBusDateTime()).isBefore(LocalDateTime.now()); // 승차 시간 검증
+        assertThat(bus.getPassengers()).isEqualTo(1); // 버스 승객수 검증
     }
 
     @Test
-    @DisplayName("버스 요금 지불 실패")
+    @DisplayName("잔액 부족 테스츠 -> IllegalArgumentException 에러 검증")
     void getOnBus_fail() {
-        // 버스 생성
-        Bus bus900 = Bus.builder().id(1L)
-                .name("마을버스")
-                .number(900)
-                .build();
+        // given
+        Bus bus900 = createBus(); // 버스 생성
+        Member member = createMember(900); // 멤버 생성
 
-        // 멤버 생성
-        Member member = Member.builder()
-                .id(1L)
-                .name("스폰지밥")
-                .money(900)
-                .build();
-
+        // when && then
         assertThatThrownBy(() -> member.getOnBus(bus900))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("잔액이 부족합니다.");
+                .hasMessage("잔액이 부족합니다."); // 잔액 부족 에러 검증
     }
 
+    @Test
+    @DisplayName("버스 하차 -> 잔액, 승객 수, 하차 시간")
+    void getOffBus() {
+        // given
+        Member member = createMember(10000); // 멤버 생성
+        Bus bus = createBus(); // 버스 생성
+
+        // when
+        member.getOnBus(bus); // 승차
+        member.getOffBus(bus); // 하차
+
+        // then
+        assertThat(member.getMoney()).isEqualTo(9000); // 잔액 검증
+        assertThat(bus.getPassengers()).isEqualTo(0); // 숭객수 검증
+        assertThat(member.getGetOffTheBusDateTime()).isBefore(LocalDateTime.now()); // 하차 시간 검증
+    }
+
+
+
+    private Member createMember(int money) {
+        return Member.builder()
+                .id(1L)
+                .name("스폰지밥")
+                .money(money)
+                .build();
+    }
+
+    private Bus createBus() {
+        return Bus.builder().id(1L)
+                .name("마을버스")
+                .number(900)
+                .build();
+    }
 }
