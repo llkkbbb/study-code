937ac83750ba03e3a6aa45c08d21d62d37c3a83a
이경범 <tkdwk567@naver.com>
2022-07-27 14:05:44 +0900

    item 24 # 멤버 클래스는 되도록 static 으로 만들라 study md upload

---
+++ src/main/java/com/example/blogcode/effectivejava/item24/Outer.java
@@ -0,0 +1,16 @@
+package com.example.blogcode.effectivejava.item24;
+
+/**
+ * packageName    : com.example.blogcode.effectivejava.item24
+ * fileName       : Outer
+ * author         : tkdwk567@naver.com
+ * date           : 2022/07/27
+ */
+public class Outer {
+
+    private int a;
+
+    class inner {
+        private int b;
+    }
+}
