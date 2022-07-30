898d6dc70d67cbc6a7f186c64c30603ca84f79a2
이경범 <tkdwk567@naver.com>
2022-07-31 01:20:52 +0900

    async sync study

--- src/main/java/com/example/blogcode/BlogCodeApplication.java
+++ src/main/java/com/example/blogcode/BlogCodeApplication.java
@@ -3,7 +3,9 @@ package com.example.blogcode;
 import lombok.experimental.PackagePrivate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class BlogCodeApplication {
 
