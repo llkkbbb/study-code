9df63092968bddacb7540a9e2461284000d8330a
이경범 <tkdwk567@naver.com>
2022-08-05 19:37:49 +0900

    itme 26 로 타입(Raw Type)은 사용하지 말라 study md upload

--- src/main/java/com/example/blogcode/effectivejava/item25/Company.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogcode.effectivejava.item25;
-
-public class Company {
-    static final String NAME = "samsung";
-}
-
-class Product {
-    static final String NAME = "phone";
-}
