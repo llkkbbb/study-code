e94bc6dcc39799953d8f6d118a3d1ca7f89365b8
이경범 <tkdwk567@naver.com>
2022-08-06 17:14:22 +0900

    item 27 비검사 경고를 제거하라 study md upload

---
+++ src/main/java/com/example/blogcode/effectivejava/item27/CompileWarning.java
@@ -0,0 +1,12 @@
+package com.example.blogcode.effectivejava.item27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CompileWarning {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList();
+    }
+
+}
